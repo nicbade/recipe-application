2f3adaa6f0719d4629c44e9281da4a92402bf098
Nic Bade <chipchapmaniii@gmail.com>
2017-11-23 10:23:19 -0700

    first commit, setup complete

---
+++ server/public/scripts/client.js
@@ -0,0 +1,37 @@
+var myApp = angular.module('myApp', ['ngRoute']);
+
+/// Routes ///
+myApp.config(function($routeProvider, $locationProvider) {
+  $locationProvider.hashPrefix('');
+  console.log('myApp -- config')
+  $routeProvider
+    .when('/home', {
+      templateUrl: '/views/templates/home.html',
+      controller: 'LoginController as lc',
+    })
+    .when('/register', {
+      templateUrl: '/views/templates/register.html',
+      controller: 'LoginController as lc'
+    })
+    .when('/user', {
+      templateUrl: '/views/templates/user.html',
+      controller: 'UserController as uc',
+      resolve: {
+        getuser : function(UserService){
+          return UserService.getuser();
+        }
+      }
+    })
+    .when('/dashboard', {
+      templateUrl: '/views/templates/info.html',
+      controller: 'DashboardController as dc',
+      resolve: {
+        getuser : function(UserService){
+          return UserService.getuser();
+        }
+      }
+    })
+    .otherwise({
+      redirectTo: 'home'
+    });
+});
