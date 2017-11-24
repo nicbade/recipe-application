4caaa7a83e63cded6e91ba1157c6e2a2efbb2b52
Nic Bade <chipchapmaniii@gmail.com>
2017-11-23 20:49:50 -0700

    started recipe post route

--- server/public/scripts/controllers/recipe.controller.js
+++ server/public/scripts/controllers/recipe.controller.js
@@ -2,4 +2,10 @@ myApp.controller('RecipeController', function(RecipeService) {
     console.log('RecipeController created');
     var self = this;
     self.RecipeService = RecipeService;
+
+    self.addRecipe = function() {
+      console.log('addRecipe post route: ', self.recipe)
+      // RecipeService.addRecipe(self.newRecipe);
+      self.recipe = {};
+    }
   });
