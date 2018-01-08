5efe409f1476594f58bbd8efeddb7cceb69b2d28
Nic Bade <chipchapmaniii@gmail.com>
2018-01-08 12:56:44 -0600

    added for loop to filter ingredients by recipe

--- server/public/scripts/controllers/recipe.controller.js
+++ server/public/scripts/controllers/recipe.controller.js
@@ -1,18 +1,18 @@
 myApp.controller('RecipeController', ['RecipeService', '$routeParams', '$location', function (RecipeService, $routeParams, $location) {
-  console.log('RecipeController created');
+  // console.log('RecipeController created');
   var self = this;
   self.RecipeService = RecipeService;
-  console.log('$routeParams ', $routeParams);
+  // console.log('$routeParams ', $routeParams);
   // ADD RECIPE POST ROUTE
   self.addRecipe = function () {
-    console.log('addRecipe post route: ', self.recipe)
+    // console.log('addRecipe post route: ', self.recipe)
     RecipeService.addRecipe(self.recipe);
     // self.recipe = {};
     $location.path('recipeDisplay/' + RecipeService.recipe.id);
   };
 
   self.addIngredient = function () {
-    console.log('addIngredient post route: ', self.ingredients);
+    // console.log('addIngredient post route: ', self.ingredients);
     RecipeService.addIngredient(self.ingredients);
     self.ingredients = {};
   };
--- server/public/scripts/services/recipe.service.js
+++ server/public/scripts/services/recipe.service.js
@@ -1,23 +1,22 @@
 myApp.service('RecipeService', ['$http', '$location', function ($http, $location) {
-    console.log('RecipeService Loaded');
+    // console.log('RecipeService Loaded');
 
     var self = this;
     self.recipe = {
         list: []
     };
-    self.currentRecipe = {
-        list: []
-    };
+
     self.ingredient = {
         list: []
     };
 
+    self.recipeIngredient = [];
+
     // RECIPE POST ROUTE
     self.addRecipe = function (newRecipe) {
         self.newRecipe = newRecipe;
-        console.log('newRecipe: ', newRecipe);
         $http.post('/recipe', newRecipe).then(function (response) {
-            console.log('service post was returned: ', response.data);
+            // console.log('service post was returned: ', response.data);
             $location.path('recipeDisplay/' + response.data.rows[0].id);
             // PARAMS TO ADD TO DB UNDER INGREDIENT
             self.params = response.data.rows[0].id;
@@ -27,9 +26,8 @@ myApp.service('RecipeService', ['$http', '$location', function ($http, $location
     // INGREDIENT POST ROUTE
     self.addIngredient = function (newIngredient) {
         self.newIngredient = newIngredient;
-        console.log('newIngredient: ', newIngredient);
         $http.post('/recipe/ingredient/' + self.params, newIngredient).then(function (response) {
-            console.log('service post was returned: ', response);
+            // console.log('service post was returned: ', response);
             self.getIngredient();
         });
     } // END INGREDIENT POST ROUTE
@@ -37,7 +35,7 @@ myApp.service('RecipeService', ['$http', '$location', function ($http, $location
     // RECIPE GET ROUTE
     self.getRecipe = function () {
         $http.get('/recipe').then(function (response) {
-            console.log('get route: ', response.data);
+            // console.log('get route: ', response.data);
             self.recipe.list = response.data;
         });
     } // END GET RECIPE ROUTE
@@ -47,7 +45,17 @@ myApp.service('RecipeService', ['$http', '$location', function ($http, $location
         $http.get('/recipe/ingredient').then(function (response) {
             console.log('get ingredient route: ', response.data);
             self.ingredient.list = response.data;
-            console.log('id: ', self.ingredient.list)
+            console.log('self.ingredient.list', self.ingredient.list);
+            console.log('self.ingredient.list.recipe_id ', self.ingredient.list[0].id);
+            console.log('self.params ', self.params);
+            for(i = 0; i < self.ingredient.list.length; i++) {
+                if(self.ingredient.list[i].id === self.params) {
+                    self.recipeIngredient.push(self.ingredient.list[i]);
+                } 
+                
+            }
+            console.log('did it work?', self.recipeIngredient);
+
         });
     } // END INGREDIENT GET ROUTE
 }]);
